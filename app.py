58bb1987ab26930687f550528e45154543baf964
karlitisTV3 <dambergskarlis@gmail.com>
2026-04-28 15:42:23 +0300

    27.04.2026 un 28.04.2026

--- app.py
+++ app.py
@@ -11,24 +11,76 @@ app.secret_key = "slepenais"
 def get_db():
     return sqlite3.connect("datubaze.db")
 
+
 @app.route("/")
 def home():
     return render_template("index.html")
 
 
-@app.route("/login", methods=["GET", "POST"])
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-    return render_template("login.html")
+
+    if request.method == 'POST':
+        epasts = request.form.get('epasts')
+        parole = request.form.get('parole')
+
+        conn = sqlite3.connect("datubaze.db")
+        conn.row_factory = sqlite3.Row
+        cur = conn.cursor()
+        cur.execute("SELECT * FROM users WHERE epasts = ?", (epasts,))
+        lietotajs = cur.fetchone()
+        conn.close()
+
+        if lietotajs and check_password_hash(lietotajs['parole'], parole):
+            session['id'] = lietotajs['id']
+            session['vards'] = lietotajs['vards']
+            return redirect('/izvelne')
+
+        else:
+            return "Nepareizi dati"
+
+    return render_template('login.html')
 
 
-@app.route("/register", methods=["GET", "POST"])
+@app.route('/register', methods=['GET', 'POST'])
 def register():
-    return render_template("register.html")
+
+    if  request.method == 'POST':
+        epasts = request.form.get('epasts')
+        vards = request.form.get('vards')
+        uzvards = request.form.get('uzvards')
+        klase = request.form.get('klase')
+        parole = request.form.get('parole')
+        parole_hash = generate_password_hash(parole)
+
+        conn = sqlite3.connect("datubaze.db")
+        cur = conn.cursor()
+
+        cur.execute("""
+        INSERT INTO users (epasts, vards, uzvards, klase, parole)
+        VALUES (?, ?, ?, ?, ?)
+        """, (epasts, vards, uzvards, klase, parole_hash))
+
+        conn.commit()
+        conn.close()
+
+        return redirect('/login')
+
+    return render_template('register.html')
 
 
 @app.route("/izvelne")
 def izvelne():
-    return render_template("izvelne.html")
+
+    conn = sqlite3.connect("datubaze.db")
+    conn.row_factory = sqlite3.Row
+    cur = conn.cursor()
+
+    cur.execute("SELECT * FROM pulcini")
+    pulcini = cur.fetchall()
+    conn.close()
+
+    return render_template("izvelne.html" , pulcini=pulcini)
 
 
 @app.route("/pieteikumi")
@@ -36,9 +88,25 @@ def pieteikumi():
     return render_template("pieteikumi.html")
 
 
-@app.route("/admin")
+@app.route('/admin')
 def admin():
-    return render_template("admin.html")
+
+    conn = sqlite3.connect("datubaze.db")
+    conn.row_factory = sqlite3.Row
+    cur = conn.cursor()
+
+    cur.execute("SELECT * FROM pulcini")
+    pulcini = cur.fetchall()
+    cur.execute("""
+    SELECT pieteikumi.id, users.vards, users.uzvards, users.klase, pulcini.nosaukums
+    FROM pieteikumi
+    JOIN users ON pieteikumi.user_id = users.id
+    JOIN pulcini ON pieteikumi.pulcins_id = pulcini.id
+    """)
+    pieteikumi = cur.fetchall()
+    conn.close()
+
+    return render_template('admin.html', pulcini=pulcini, pieteikumi=pieteikumi)
 
 
 @app.route("/logout")
